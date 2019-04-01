c57109ed0753f15f1299c66d31460d32bd0b86cc
Diego Forero <diego.n.forero@gmail.com>
2019-03-31 23:10:22 -0500

    entrega funcional dom

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1,44 +1,43 @@
 const hbs=require('hbs');
 
-// hbs.registerHelper('obtenerPromedio',(nota1,nota2,nota3)=>{
-//     return (nota1+nota2+nota3)/3;
-// });
+hbs.registerHelper('resultadoDeCreacion',()=>{
+    const funciones=require('./../funciones')
+    if(!funciones.crear){
+        resultado='Curso creado con éxito!'
+    } else {
+        resultado='Curso ya existe!'+'<br> Intente nuevamente'
+    }
+    return(resultado)
+})
 
 hbs.registerHelper('listar',()=>{
-    listaCursos=require('./../listado.json')
-    let texto =
-    '<table style="text-align:center;">\
-        <thead>\
-            <th>Id</th>\
-            <th>Nombre</th>\
-            <th>Modalidad</th>\
-            <th>Valor</th>\
-            <th>Descripcion</th>\
-            <th>Intensidad</th>\
-        </thead>\
-    <tbody>'
+    listaCursos=require('./../../listado.json')
+    let texto = "<table class='table table-striped table-hover'> \
+        <thead class='thead-dark'> \
+            <th scope='col'>Id</th> \
+            <th scope='col'>Nombre</th> \
+            <th scope='col'>Descripcion</th> \
+            <th scope='col'>Valor</th> \
+            <th scope='col'>Modalidad</th> \
+            <th scope='col'>Intensidad</th> \
+            <th scope='col'>Estado</th> \
+        </thead> \
+    <tbody>";
 
 
-    listaCursos.forEach(estudiante => {texto=
+    listaCursos.forEach(curso => {texto=
         texto+
-        '<tr>'+
-        '<td>'+estudiante.nombre+'</td>'+
-        '<td>'+estudiante.matematicas+'</td>'+
-        '<td>'+estudiante.ingles+'</td>'+
-        '<td>'+estudiante.programacion+'</td>'+
+         "<tr> "+
+        '<td>'+curso.id+'</td>'+
+        '<td>'+curso.nombre+'</td>'+
+        '<td>'+curso.descripcion+'</td>'+
+        '<td>'+curso.valor+'</td>'+
+        '<td>'+curso.modalidad+'</td>'+
+        '<td>'+curso.intensidad+'</td>'+
+        '<td>'+curso.estado+'</td>'+
         '</tr>';
-    });
-    texto=texto+'</tbody></table>'
-    
+    })
+    texto=texto+'</tbody></table>';
 
-
-    // ' Lista de Esstudi4ntes'+' <br> ';
-    // listaCursos.forEach(estudiante => {
-    //     texto=texto+
-    //         "Nombre: "+estudiante.nombre +'<br>'+
-    //         "Matemáticas: "+estudiante.matematicas+'<br>'+
-    //         "Ingles: "+estudiante.ingles+'<br>'+
-    //         "Programacion: "+estudiante.programacion+'<br>'
-    // })
-    return(texto);
-})//en este caso nor equiere arg de entrada porque se va a calcular automaticamente con lo que se tenga en el json
+    return texto;
+})
--- src/start.js
+++ src/start.js
@@ -9,19 +9,11 @@ const funciones = require ('./funciones')
 
 const directorioviews=path.join(__dirname,'../template/views')
 
-// app.set("views",)
+
 app.set('views', directorioviews );
-//__dirname=C:\Users\Diego Forero\Desktop\DIPLOMADO NODE\bootstrap\src
-//directoriopublico=C:\Users\Diego Forero\Desktop\DIPLOMADO NODE\bootstrap\public
-//directoriopartials=C:\Users\Diego Forero\Desktop\DIPLOMADO NODE\bootstrap\template
 
-
-// app.use('views', path.join(__dirname, '../template/'));
-
-
-// const test=path.join(path,'')
 const directoriopublico=path.join(__dirname,'../public')
-// const directoriopartials=path.join(__dirname,'../partials')
+
 const directoriopartials=path.join(__dirname,'../template/partials')
 app.use(express.static(directoriopublico));
 hbs.registerPartials(directoriopartials)
@@ -31,21 +23,21 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.set('view engine','hbs')
 
 app.get(['/','/index'],(req,res)=>{
-    // res.send('RESPUESTA DESDE INDEX');
+    
     res.render('index',{
         estudiante: 'Diego'
     })
 })
 
 app.get('/crear',(req,res)=>{
-    // res.send('RESPUESTA DESDE INDEX');
+    
     res.render('crear',{
         estudiante: 'Diego'
     })
 })
 
 app.post('/creacion',(req,res)=>{
-    // res.send('RESPUESTA DESDE CALCULOS');
+    
     res.render('creacion',{
         id: parseInt(req.body.idfield),
         nombre: req.body.coursename,
@@ -71,6 +63,11 @@ app.post('/creacion',(req,res)=>{
 
 })
 
+app.get('/listar',(req,res)=>{
+    res.render('listar',{
+
+    })
+})
 
 
 
